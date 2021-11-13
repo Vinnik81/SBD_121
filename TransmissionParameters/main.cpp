2c95d70b220cd3a73674d12bc0e4f8e9c8137301
ВинБоцы ВинБоцы <solarisraver@mail.ru>
2021-11-13 20:26:33 +0600

    Parametres by value

--- TransmissionParameters/main.cpp
+++ TransmissionParameters/main.cpp
@@ -8,10 +8,12 @@ void main()
 	setlocale(LC_ALL, "Russian");
 	int a = 2, b = 3;
 	Exchange(a, b);
-	cout << a << b << endl;
+	cout << a << "\t" << b << endl;
 }
 void Exchange(int a, int b)
 {
+	cout << &a << "\t" << &b << endl;
+
 	int buffer = a;
 	a = b;
 	b = buffer;
